db600746f47e92d3f74632f24fddbeee05dc0cb9
LeJammes454 <s19120178@alumnos.itsur.edu.mx>
2023-05-01 13:42:16 -0600

    Agragando funconalidad al mapa. agregando localizacion del usuario

--- app/src/main/java/com/example/proyectomapasgeo/MainActivity.kt
+++ app/src/main/java/com/example/proyectomapasgeo/MainActivity.kt
@@ -1,11 +1,90 @@
 package com.example.proyectomapasgeo
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestPermissionsIfNecessary()
+        Configuration.getInstance().load(applicationContext, getSharedPreferences("osmdroid", MODE_PRIVATE))
+
+        mapView = findViewById(R.id.map_view)
+
+        mapView.setTileSource(TileSourceFactory.MAPNIK)
+        mapView.setMultiTouchControls(true)
+        mapView.zoomController.setVisibility(org.osmdroid.views.CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
+
+        val mapController = mapView.controller
+        mapController.setZoom(15.0)
+
+
+        /*
+        Establecer una ubicación inicial del mapa
+        val startPoint = GeoPoint(20.138733, -101.150324) // Ubicacion del ITSUR
+        mapController.setCenter(startPoint)
+
+        val myLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mapView)
+        myLocationOverlay.enableMyLocation()
+        mapView.overlays.add(myLocationOverlay)
+         */
+
+        val myLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mapView)
+        myLocationOverlay.enableMyLocation()
+        myLocationOverlay.enableFollowLocation()
+        mapView.overlays.add(myLocationOverlay)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    private fun requestPermissionsIfNecessary() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            val permissions = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_NETWORK_STATE
+            )
+
+            val missingPermissions: MutableList<String> = ArrayList()
+            for (permission in permissions) {
+                if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    missingPermissions.add(permission)
+                }
+            }
+
+            if (missingPermissions.isNotEmpty()) {
+                ActivityCompat.requestPermissions(this, missingPermissions.toTypedArray(), REQUEST_PERMISSIONS_REQUEST_CODE)
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_PERMISSIONS_REQUEST_CODE = 1
     }
 }
+
